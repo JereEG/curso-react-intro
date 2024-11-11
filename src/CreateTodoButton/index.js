9510e4714bf45e6cbfdff7c6d00e26710a46102f
Jeremias Gonzalez <jeremiasgonzalez7464@gmail.com>
2024-11-11 20:44:15 -0300

    add funcionalidad en button agregar todo

--- src/CreateTodoButton/index.js
+++ src/CreateTodoButton/index.js
@@ -1,10 +1,14 @@
 import "./CreateTodoButton.css";
+import React from "react";
+import { TodoContext } from "../TodoConetext";
 
 function CreateTodoButton() {
+  const { openModal, setOpenModal } = React.useContext(TodoContext);
   return (
     <button
       className="CreateTodoButton"
       onClick={(event) => {
+        setOpenModal(!openModal);
         console.log("le diste click");
         console.log(event);
         console.log(event.target);
